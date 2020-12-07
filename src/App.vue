302575e637d6c9e85adf0272fc766f362b8e18d5
riyadhossein@gmail.com <riyadhossein@gmail.com>
2020-12-07 14:58:08 +0600

    vue project for practice

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <p>{{title}}</p>
+    <navbar></navbar>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import navbar from './components/navbar.vue'
+export default {
+  name: 'App',
+
+  data(){
+    return{
+      title : "First vue app"
+    }
+  },
+
+  components:{
+    navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="nav">
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : "navbar"
+    }   
+</script>
+
+<style scoped>
+    .nav ul{display: flex;align-items: center;justify-content: center;}
+    .nav ul li{ list-style: none; padding: 0px 20px;}
+
+</style>
